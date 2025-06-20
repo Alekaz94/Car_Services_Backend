3b09041f56bd8b3760e045b0114aef4300933b69
Alexandros <alexandros.kazalis@hotmail.com>
2025-06-20 21:31:11 +0200

    Created a post method for creating a new user. checks if a user has the email that new user is trying to create. if no user has that email it then creates a new user.

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,26 +1,50 @@
 package com.example.demo.Controllers;
 
 import com.example.demo.Entities.User;
+import com.example.demo.Repositories.UserRepository;
 import com.example.demo.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController("/api/v1")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/user/{userId}")
     public User getUser(@PathVariable UUID userId) {
         return userService.getUser(userId);
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(User user) {
+        Optional<User> foundUserWithEmail = userRepository.findByEmail(user.getEmail());
+
+        if(foundUserWithEmail.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            return userService.createUser(user);
+        }
     }
 }
--- src/main/java/com/example/demo/Services/UserService.java
+++ src/main/java/com/example/demo/Services/UserService.java
@@ -3,8 +3,11 @@ package com.example.demo.Services;
 import com.example.demo.Entities.User;
 import com.example.demo.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -37,4 +40,16 @@ public class UserService {
         }
     }
 
+    public ResponseEntity<User> createUser(User user) {
+
+        User userToCreate = new User();
+
+        userToCreate.setFirstName(user.getFirstName());
+        userToCreate.setLastName(user.getLastName());
+        userToCreate.setEmail(user.getEmail());
+        userToCreate.setAge(user.getAge());
+
+        userRepository.save(userToCreate);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
