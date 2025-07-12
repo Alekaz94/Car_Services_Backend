97fa660254d051fcae7513c95743e5595ab1fc6d
Alexandros <alexandros.kazalis@hotmail.com>
2025-07-12 17:42:53 +0200

    Creates a admin user at the start of the project.

--- src/main/java/com/example/demo/CarServiceBackendApplication.java
+++ src/main/java/com/example/demo/CarServiceBackendApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.Entities.User;
+import com.example.demo.Enums.Roles;
+import com.example.demo.Repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CarServiceBackendApplication {
@@ -10,4 +16,19 @@ public class CarServiceBackendApplication {
 		SpringApplication.run(CarServiceBackendApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (userRepository.findByEmail("admin.admin@work.com").isEmpty()) {
+				User adminUser = new User();
+				adminUser.setFirstName("Admin");
+				adminUser.setLastName("Admin");
+				adminUser.setEmail("admin.admin@work.com");
+				adminUser.setPassword(passwordEncoder.encode("Admin123"));
+				adminUser.setRole(Roles.ADMIN);
+
+				userRepository.save(adminUser);
+			}
+		};
+	}
 }
